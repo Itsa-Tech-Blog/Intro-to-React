62038d27702acfe336d455805a3613eb4dd3a470
JDeLay-JR <jdelay.jr@gmail.com>
2018-12-05 21:44:35 -0500

    fixing code example

--- programatically-render-data/src/components/SideBar/SideBar.js
+++ programatically-render-data/src/components/SideBar/SideBar.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './SideBar.css'
 
-const SideBar = () => {
-    let menuItems = ['Home', "About", 'Articles', 'More']
-    return (
-        <div className="menu-container">
-            <h1>Nav Menu</h1>
-            <ul>
-                {menuItems.map((menuItem) => {
-                    return (
-                        <li key={menuItem} onClick={() => alert(`You clicked ${menuItem}!`)}>{menuItem}</li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+
+class SideBar extends Component {
+    render() {
+        let menuItems = ['Home', "About", 'Articles', 'More']
+        return (
+            <div className="menu-container">
+                <h1>Nav Menu</h1>
+                <ul>
+                    {menuItems.map((menuItem) => {
+                        return (
+                            <li key={menuItem} onClick={() => alert(`You clicked ${menuItem}!`)}>{menuItem}</li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default SideBar;
